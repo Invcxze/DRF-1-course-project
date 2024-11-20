7f13b33596c38b6095959da954608c1bbfa197f3
Khramenko Vladislav Vladimirovich <vladhromenko@gmail.com>
2024-11-20 20:24:17 +0300

    fix settings.py

--- ShopApi/ShopApi/settings.py
+++ ShopApi/ShopApi/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'corsheaders.middleware.CorsMiddleWare',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 CORS_ORIGIN_WHITELIST = [
