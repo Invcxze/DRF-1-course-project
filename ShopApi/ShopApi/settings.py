1390a785a100c76526e018f439ee9babf8e10477
Khramenko Vladislav Vladimirovich <vladhromenko@gmail.com>
2024-11-24 11:56:32 +0300

    fix settings.py

--- ShopApi/ShopApi/settings.py
+++ ShopApi/ShopApi/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'shop',
-    'users',
+    'shop.apps.ShopConfig',
+    'users.apps.UserConfig',
     'corsheaders',
 ]
 
